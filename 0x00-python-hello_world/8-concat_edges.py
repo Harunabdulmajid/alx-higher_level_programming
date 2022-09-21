e9d812e99729bc5b4171607fec5adc753b8d253b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-09-21 23:34:50 +0000

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str1 = "object-oriented programming"
+str2 = " with python"
+str = str1 + str2
 print(str)
