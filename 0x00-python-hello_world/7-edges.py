16c205e20353685c31ae1a270a1d89d3705292b6
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-09-21 19:44:08 +0000

    9-easter_egg.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,7 @@
+
+rd = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
