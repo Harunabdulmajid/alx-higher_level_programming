cae1e1c40976c6bda429764840f60ce2f3741d62
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-09-26 23:12:47 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
 	sentence = ("At school, I learnt C!")
-print("The length is {:d} - first character: {}", len(sentence))
+print("The length is :", len(sentence))
