e7f270558b3d8acf9ab47fcc8fa62811aa0d92c2
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-09-26 22:28:47 +0000

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list):
-	my_list = "{}\n{}\n{}\n{}"
-print(my_list.format(0, 1, 2, 3, 4))
-
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
