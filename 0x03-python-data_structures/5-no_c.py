2be4752243ca2ad9d433a34fcc82c1b1bd9f0cde
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-09-27 00:07:41 +0000

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def no_c(my_string):
-	my_string = ("A, b, C, c, D, E")
-print("my_string")
-
+def no_c(my_string)
+	s = 'abCcdddee'
+print(s.translate({ord(i): None for i in 'Cc'}))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
 	my_list = [12, 45, 78, 9]
 maximum_number = my_list[0]
 for num in my_list:
-	if num > maximum_number:
-	maximum_number = num
+if num > maximum_number:
+maximum_number = num
 print(maximum_number)
