efae90e2adc248f767b2332229abe9de331094f7
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-09-27 20:33:00 +0000

    1-element_at.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	s = 'abCcdddee'
-print(s.translate({ord(i): None for i in 'Cc'}))
+	no_c = 'abCcdddee'
+print(no_c.translate({ord(i): None for i in 'Cc'}))
